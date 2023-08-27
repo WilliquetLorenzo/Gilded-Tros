1fd2c9b7f5846432832c3afa9bb60d6797dc8559
WilliquetLorenzo <williquet.lorenzo@gmail.com>
2023-08-27 17:52:06 +0200

    Smelly items can work with the system. Added some extra methods that adjust the quality of an item with some checks on it.

--- CSharp/GildedTros.App/GildedTros.cs
+++ CSharp/GildedTros.App/GildedTros.cs
@@ -8,6 +8,9 @@ namespace GildedTros.App
         private const string B_DAWG_KEYCHAIN = "B-DAWG Keychain";
         private const string BACKSTAGE_PASS_RE_FACTOR = "Backstage passes for Re:factor";
         private const string BACKSTAGE_PASS_HAXX = "Backstage passes for HAXX";
+        private const string DUPLICATE_CODE = "Duplicate Code";
+        private const string LONG_METHODS = "Long Methods";
+        private const string UGLY_VARIABLE_NAMES = "Ugly Variable Names";
 
         IList<Item> Items;
         public GildedTros(IList<Item> Items)
@@ -20,7 +23,6 @@ namespace GildedTros.App
             // Simpele iteratie door een lijst
             foreach (Item item in Items)
                 UpdateItemQuality(item);
-
         }
 
         private void UpdateItemQuality(Item item)
@@ -34,15 +36,20 @@ namespace GildedTros.App
                 {
                     if (item.Name != B_DAWG_KEYCHAIN)
                     {
-                        item.Quality = item.Quality - 1;
+                        if (item.Name == DUPLICATE_CODE || item.Name == LONG_METHODS || item.Name == UGLY_VARIABLE_NAMES)
+                            AdjustItemQuality(item, -2);
+
+                        else
+                            AdjustItemQuality(item, -1);
                     }
+                   
                 }
             }
             else
             {
                 if (item.Quality < 50)
                 {
-                    item.Quality = item.Quality + 1;
+                    AdjustItemQuality(item, 1);
 
                     if (item.Name == BACKSTAGE_PASS_RE_FACTOR
                     || item.Name == BACKSTAGE_PASS_HAXX)
@@ -51,7 +58,7 @@ namespace GildedTros.App
                         {
                             if (item.Quality < 50)
                             {
-                                item.Quality = item.Quality + 1;
+                                AdjustItemQuality(item, 1);
                             }
                         }
 
@@ -59,7 +66,7 @@ namespace GildedTros.App
                         {
                             if (item.Quality < 50)
                             {
-                                item.Quality = item.Quality + 1;
+                                AdjustItemQuality(item, 1);
                             }
                         }
                     }
@@ -82,23 +89,57 @@ namespace GildedTros.App
                         {
                             if (item.Name != B_DAWG_KEYCHAIN)
                             {
-                                item.Quality = item.Quality - 1;
+                                if (item.Name == DUPLICATE_CODE || item.Name == LONG_METHODS || item.Name == UGLY_VARIABLE_NAMES)
+                                    AdjustItemQuality(item, -2);
+
+                                else
+                                    AdjustItemQuality(item, -1);
                             }
                         }
                     }
                     else
                     {
-                        item.Quality = item.Quality - item.Quality;
+                        AdjustItemQuality(item, -1);
                     }
                 }
                 else
                 {
                     if (item.Quality < 50)
                     {
-                        item.Quality = item.Quality + 1;
+                        AdjustItemQuality(item, 1);
                     }
                 }
             }
+        }
+
+        private void AdjustItemQuality(Item item, int adjustment)
+        {
+            int newItemQuality = item.Quality + adjustment;
+
+            if (IsItemQualityBiggerThenFifthy(newItemQuality) == false && IsItemQualitySmallerThenZero(newItemQuality) == false)
+                item.Quality = newItemQuality;
+
+            else if (IsItemQualityBiggerThenFifthy(newItemQuality))
+                item.Quality = 50;
+
+            else if (IsItemQualitySmallerThenZero(newItemQuality)) 
+                item.Quality = 0;
+        }
+
+        private bool IsItemQualityBiggerThenFifthy(int item)
+        {
+            if(item > 50)
+                return true;
+
+            return false;
+        }
+
+        private bool IsItemQualitySmallerThenZero(int item)
+        {
+            if (item < 0)
+                return true;
+
+            return false;
         }
     }
 }
